5299283ccfb8ebb33339f56c2036fe5cb6bfb9ef
nick <nicktanaka36@gmail.com>
2024-08-01 09:52:53 -0700

    implement user auth

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,6 @@
+from flask import redirect, url_for, request
 from flask_mysqldb import MySQL 
+import requests
 from app import create_app
 
 app = create_app()
@@ -6,15 +8,25 @@ mysql = MySQL(app)
 
 @app.route("/", methods=['GET'])
 def home_page():
-    return "<p>hello world</p>"
+    return redirect(url_for("auth"))
 
-@app.route("/auth", methods=['GET'])
+@app.route("/auth", methods=['POST'])
 def auth():
+    data = request.json
+    email = data['email']
+    password = data['password']
+        
     cur = mysql.connect.cursor()
-    query = "SELECT * FROM tablename"
-    res = cur.execute(query)
-    print(res)
-    return "fklasjflkasjfls"
+    query = f"SELECT password FROM Users WHERE email='{email}'"
+    cur.execute(query)
+    res = cur.fetchone()
+       
+    payload = {"auth_message": False}
+    if res['password'] == password:
+        payload = {"auth_message": True}
+    # requests.post("http://classwork.engr.oregonstate.edu:61932", json = payload)
+    return payload
+
 
 if __name__ == '__main__':
     app.run(debug=True)
